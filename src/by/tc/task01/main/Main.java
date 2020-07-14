e2f04b7a4f7daf201849ab53abe18d6a8f6433bb
HP <HP@DESKTOP-JDNAHGL>
2020-07-14 23:23:38 +0300

    Version 1.1

--- src/by/tc/task01/main/Main.java
+++ src/by/tc/task01/main/Main.java
@@ -25,7 +25,6 @@ public class Main {
 			appliances = service.find(criteriaOven);
 		} catch (ServiceException e) {
 			e.printStackTrace();
-			;
 		}
 
 		PrintListApplianceInfo.print(appliances);
